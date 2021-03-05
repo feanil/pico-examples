668a280b5cdabda777eb6c3c5febece89a487825
Feanil Patel <feanil@gmail.com>
2021-03-04 22:10:50 -0500

    feat: Simple ADC to serial.
    Simplify what's being printed and take samples more
    often.
    
    This is the version loaded onto 3 boards on
    March 4, 2021

--- eclipsemob/eclipsemob.c
+++ eclipsemob/eclipsemob.c
@@ -21,9 +21,8 @@ int main() {
 
     while (1) {
         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-        const float conversion_factor = 3.3f / (1 << 12);
         uint16_t result = adc_read();
-        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
-        sleep_ms(500);
+        printf("%03x\n", result);
+        sleep_us(5);
     }
 }
